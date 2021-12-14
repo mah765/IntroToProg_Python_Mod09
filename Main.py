f22d4d755f7acc399f271a24c0956ec79ce4dde7
mah765 <74583008+mah765@users.noreply.github.com>
2021-12-14 09:02:25 -0800

    Update Main.py
    Updated comments.

--- Main.py
+++ Main.py
@@ -1,12 +1,21 @@
 # ------------------------------------------------------------------------ #
 # Title: Assignment 09
-# Description: Working with Modules
+# Description: Working with Modules. This application includes several modules:
+# (1) the main module, (2) a DataClasses module which defines the Person and
+# Employee objects, (3) the ProcessingClasses that include methods for processing
+# the data, and (4) the IOClasses which include methods for reading/writing data
+# and collecting user input. The application first reads in data from an existing
+# file, and then displays a menu to the user who can choose to display these items,
+# to add a new item (employee), to save to file, or to exit. The script includes
+# error handling for cases when blank lines appear in the input file, or when the
+# user tries to enter an invalid entry for a new employee.
 
 # ChangeLog (Who,When,What):
 # RRoot,1.1.2030,Created started script
 # RRoot,1.1.2030,Added pseudo-code to start assignment 9
 # MHamilton,12.12.2021,Modified code to complete assignment 9
 # MHamilton,12.13.2021,Major updates
+# MHamilton,12.14.2021,Updated comments
 # ------------------------------------------------------------------------ #
 
 # Import modules
@@ -28,6 +37,7 @@ for row in lstFileData:
         print(e.to_string().strip(), type(e))
         print() # for looks
     except:
+        # Note this will print if an empty line is found.
         print("<Invalid entry found.>")
 
 while (True):
@@ -47,11 +57,12 @@ while (True):
             emp = Eio.input_employee_data()
             lstTable.append(emp)
         except:
+            # This will occur if the object does contain valid characters.
             print("Invalid entry. Try again.")
         continue
 
     elif choice_str.strip() == '3':
-        # let user save current data to file
+        # Let user save current data to file
         P.FileProcessor.save_data_to_file("EmployeeData.txt", lstTable)
         print("Data saved to file!")
         continue
